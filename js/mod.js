f050ead55591f2acb2096242102c43bbeccbcaf8
Summerlovesyou <133274783+Summerlovesyou@users.noreply.github.com>
2023-05-11 21:51:08 +0100

    Update mod.js
    Test commit changing the modInfo Object.

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The ChiChi Tree",
+	id: "ChiChiTree",
+	author: "Summerlovesyou",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
